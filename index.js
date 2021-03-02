99daec387489979ba67d3587daeb3d9c64fd9699
Regan-Mclachlan <reganmc96@hotmail.com>
2021-03-02 23:50:05 +1000

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solutions in this file
+
+const cards = ["Ada", "Brendan", "Ali"];
+function writeCards(cards) { for (let i = 0; i < cards.length; i++)  
+    {console.log(`Thank you ${cards[i]} for the wonderful birthday gift!`)}
+    return cards }
+    writeCards(cards);
+
+
+
+
+let countDown = 10
+while (countDown > -1)
+{console.log(countDown--)}
